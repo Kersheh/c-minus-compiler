27d90546d7b0febfb5a993412b52193b9161a46f
Matthew Breckon <mbreckon@mail.uoguelph.ca>
2016-03-07 10:30:20 -0500

    Added relational operators to OpExp class.

--- absyn/OpExp.java
+++ absyn/OpExp.java
@@ -5,10 +5,13 @@ public class OpExp extends Exp {
   public final static int MINUS = 1;
   public final static int TIMES = 2;
   public final static int OVER  = 3;
-  public final static int EQ    = 4;
-  public final static int LT    = 5;
+  public final static int LT    = 4;
+  public final static int LTEQ  = 5;
   public final static int GT    = 6;
-
+  public final static int GTEQ  = 7;
+  public final static int EQ    = 8;
+  public final static int NOTEQ = 9;
+  
   public Exp left;
   public int op;
   public Exp right;
