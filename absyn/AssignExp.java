06aa729a55644c9284602e98e1833cd24ebd3ba5
Matthew Breckon <mbreckon@mail.uoguelph.ca>
2016-03-07 17:44:03 -0500

    Added and modified tree node classes.

--- absyn/AssignExp.java
+++ absyn/AssignExp.java
@@ -3,7 +3,7 @@ package absyn;
 public class AssignExp extends Exp {
   public VarExp lhs;
   public Exp rhs;
-  public AssignExp( int pos, VarExp lhs, Exp rhs ) {
+  public AssignExp(int pos, VarExp lhs, Exp rhs) {
     this.pos = pos;
     this.lhs = lhs;
     this.rhs = rhs;
--- absyn/IntExp.java
+++ absyn/IntExp.java
@@ -2,7 +2,7 @@ package absyn;
 
 public class IntExp extends Exp {
   public String value;
-  public IntExp( int pos, String value ) {
+  public IntExp(int pos, String value) {
     this.pos = pos;
     this.value = value;
   }
