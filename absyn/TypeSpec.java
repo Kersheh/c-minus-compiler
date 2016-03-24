f3f1c0b9e085bac71c5b7482e642b1898822ded5
DeanWay <deanway93@gmail.com>
2016-03-23 20:22:09 -0400

    avoid string literals. erro prone

--- absyn/TypeSpec.java
+++ absyn/TypeSpec.java
@@ -2,6 +2,9 @@ package absyn;
 
 public class TypeSpec extends Absyn {
   public String type;
+  public static final String INT = "INT";
+  public static final String VOID = "VOID";
+
   public TypeSpec(int pos, String type) {
     this.pos = pos;
     this.type = type;
